659f4b24f015175ee33ebf78b7bd00766158827a
Fransiska <fransiska23ci12@gmail.com>
2026-06-21 17:36:37 +0700

    feat: add user profile feature

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function edit()
+    {
+        $user = auth()->user();
+
+        return view('user.edit', compact('user'));
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users,email,' . auth()->id()
+        ]);
+
+        $user = User::find(auth()->id());
+
+        $user->update(
+            $request->only('name', 'email')
+        );
+
+        return back()
+            ->with('success', 'Profil berhasil diupdate');
+    }
 }
