339fa5a80b149baeedf0ec394653debcc65dec9d
Thender9595 <ultramanx.desu@gmail.com>
2026-06-21 15:27:46 +0700

    Add model relationships for cart, wishlist, and product variants

--- app/Models/ProductVariant.php
+++ app/Models/ProductVariant.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductVariant extends Model
 {
-    //
+    protected $fillable = ['product_id', 'name', 'stock', 'price'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function cartItems()
+    {
+        return $this->hasMany(CartItem::class, 'product_variant_id');
+    }
 }
