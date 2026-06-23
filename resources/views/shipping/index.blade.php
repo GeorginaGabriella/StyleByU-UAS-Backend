1d004ff95f58aafd53ab8bc79aba8602f23399b1
GeorginaGabriella <gjina.grlla21@gmail.com>
2026-06-23 16:39:39 +0700

    fix: shipping controller logic and validation

--- resources/views/shipping/index.blade.php
+++ resources/views/shipping/index.blade.php
@@ -4,6 +4,10 @@
 
 <br><br>
 
+@if(session('success'))
+    <p style="color:green">{{ session('success') }}</p>
+@endif
+
 <table border="1" cellpadding="8" cellspacing="0">
     <tr>
         <th>No</th>
@@ -11,6 +15,7 @@
         <th>Keterangan</th>
         <th>Ongkir</th>
         <th>Estimasi (Hari)</th>
+        <th>Aksi</th>
     </tr>
     @foreach($shippings as $ship)
     <tr>
@@ -19,6 +24,9 @@
         <td>{{ $ship->description }}</td>
         <td>Rp {{ number_format($ship->price, 0, ',', '.') }}</td>
         <td>{{ $ship->estimated_days }} Hari</td>
+        <td>
+            <a href="{{ route('admin.shipping.edit', $ship) }}">Ubah</a>
+        </td>
     </tr>
     @endforeach
 </table>
